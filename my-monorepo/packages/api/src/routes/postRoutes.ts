b84b9aac8b54f2df148ab58be9f504c67d86fb19
pd4mort <pablodamasor@gmail.com>
2024-07-28 18:33:46 +0200

    roles post

--- my-monorepo/packages/api/src/routes/postRoutes.ts
+++ my-monorepo/packages/api/src/routes/postRoutes.ts
@@ -1,12 +1,50 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
 import { getAllPosts, getPostById, createPost, updatePost, deletePost } from '../controllers/postController';
+import { authorize, authorizePostOwner } from '../middleware/authorization';
+import { PostBody, PostParams } from '../types/postTypes';
+
+interface GetPostByIdParams {
+  Params: PostParams;
+}
+
+interface CreatePostBody {
+  Body: PostBody;
+}
+
+interface UpdatePostParams {
+  Params: PostParams;
+  Body: Partial<PostBody>;
+}
+
+interface DeletePostParams {
+  Params: PostParams;
+}
 
 async function postRoutes(server: FastifyInstance) {
-  server.get('/posts', getAllPosts);
-  server.get('/posts/:id', getPostById);
-  server.post('/posts', createPost);
-  server.put('/posts/:id', updatePost);
-  server.delete('/posts/:id', deletePost);
+  server.get('/posts', 
+    { preHandler: [server.authenticate, authorize(['god', 'admin', 'user'])] }, 
+    getAllPosts
+  );
+
+  server.get<{ Params: PostParams }>('/posts/:id', 
+    { preHandler: [server.authenticate, authorize(['god', 'admin', 'user'])] }, 
+    getPostById
+  );
+
+  server.post<{ Body: PostBody }>('/posts', 
+    { preHandler: [server.authenticate, authorize(['god', 'admin', 'user'])] }, 
+    createPost
+  );
+
+  server.put<{ Params: PostParams; Body: Partial<PostBody> }>('/posts/:id', 
+    { preHandler: [server.authenticate, authorize(['god', 'admin', 'user']), authorizePostOwner] }, 
+    updatePost
+  );
+
+  server.delete<{ Params: PostParams }>('/posts/:id', 
+    { preHandler: [server.authenticate, authorize(['god', 'admin', 'user']), authorizePostOwner] }, 
+    deletePost
+  );
 }
 
 export default postRoutes;
